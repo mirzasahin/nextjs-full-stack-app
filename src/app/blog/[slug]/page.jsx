430d465041130a3c91d8c7ec6c9eae76b5523861
Mirza Sahin <salihmirza123@gmail.com>
2024-03-05 18:25:40 +0300

    Single blog page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import styles from "./singlePost.module.css";
+import Image from "next/image";
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/7529416/pexels-photo-7529416.jpeg"
+          alt=""
+          fill
+          className={styles.img}
+        />
+      </div>
 
-export default SinglePostPage
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+          <Image
+            src="https://images.pexels.com/photos/7529416/pexels-photo-7529416.jpeg"
+            alt=""
+            width={50}
+            height={50}
+            className={styles.avatar}
+          />
+
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Michael Jamy</span>
+          </div>
+
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.03.2024</span>
+          </div>
+        </div>
+
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
+          pariatur eum. Sunt blanditiis quod quae.
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SinglePostPage;
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
   {
@@ -45,12 +46,14 @@ const Links = () => {
           <NavLink item={{ title: "Login", path: "/login" }} />
         )}
       </div>
-      <button
-        className={styles.menuButton}
+      <Image
+      className={styles.menuButton}
+        src="/menu.png"
+        alt=""
+        width={30}
+        height={30}
         onClick={() => setOpen((prev) => !prev)}
-      >
-        Menu
-      </button>
+      />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
