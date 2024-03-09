48ccac416eb084660f1b9764416f68019a5d6c3e
Mirza Sahin <salihmirza123@gmail.com>
2024-03-09 15:29:06 +0300

    Sign in with Github and add user to database

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,8 @@
+import { connect } from "mongoose";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { connectToDb } from "./utils";
+import { User } from "./models";
 export const {
   handlers: { GET, POST },
   auth,
@@ -12,4 +15,29 @@ export const {
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+
+      if (account.provider === "github") {
+        connectToDb();
+        try {
+          const user = await User.findOne({ email: profile.email });
+
+          if (!user) {
+            const newUser = new User({
+              username: profile.login,
+              email: profile.email,
+              image: profile.avatar_url,
+            });
+
+            await newUser.save();
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+      return true;
+    },
+  },
 });
