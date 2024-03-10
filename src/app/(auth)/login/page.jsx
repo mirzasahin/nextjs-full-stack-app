42b39adc2dad7f19ee61fe7144aa1d4396d545aa
Mirza Sahin <salihmirza123@gmail.com>
2024-03-10 15:52:46 +0300

    Middleware, Auth.js and protect routes

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import styles from "./login.module.css";
 
 const LoginPage = () => {
+  /* auth?.user?.isAdmin && router.push("/") */
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,6 +5,7 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDb } from "./utils";
 import { User } from "./models";
 import bcrypt from "bcryptjs";
+import { authConfig } from "./auth.config";
 
 const login = async (credentials) => {
   try {
@@ -37,6 +38,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  ...authConfig,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
@@ -76,5 +78,6 @@ export const {
       }
       return true;
     },
+    ...authConfig.callbacks,
   },
 });
